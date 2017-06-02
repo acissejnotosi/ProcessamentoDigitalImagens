234d25de5d3ed89905dd071678ef273bcf3d6fdc
allanpatrick18 <allanpatrick18@gmail.com>
2017-06-02 01:27:02 -0300

    test MVC model

---
+++ src/main/java/teste/rest/Service/AccountService.java
@@ -0,0 +1,7 @@
+package teste.rest.Service;
+
+/**
+ * Created by allan on 02-06-2017.
+ */
+public interface AcountService {
+}
---
+++ src/main/java/teste/rest/Service/AccountServiceImpl.java
@@ -0,0 +1,10 @@
+package teste.rest.Service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by allan on 02-06-2017.
+ */
+@Service
+public class AcountServiceImpl implements AcountService{
+}
